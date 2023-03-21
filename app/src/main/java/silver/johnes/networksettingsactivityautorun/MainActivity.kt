f0b664a4ecb8ebaf4d94d02d28ca24ef2c5164ea
silverjohnes <silver.johnes@gmail.com>
2023-03-21 10:35:21 +0300

    works

--- app/src/main/java/silver/johnes/networksettingsactivityautorun/MainActivity.kt
+++ app/src/main/java/silver/johnes/networksettingsactivityautorun/MainActivity.kt
@@ -2,23 +2,19 @@ package silver.johnes.networksettingsactivityautorun
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.Settings
+//import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Открываем активити Сеть настроек системы
-        //startActivity(Intent(Settings.ACTION_SETTINGS))
 
-        // Открываем Internet Conectivity
         startActivity(Intent(android.provider.Settings.Panel.ACTION_INTERNET_CONNECTIVITY))
 
         // или другие варианты:
         //startActivity( Intent(android.provider.Settings.ACTION_WIFI_SETTINGS))
+        //startActivity(Intent(Settings.ACTION_SETTINGS))
 
-
-        // Закрываем текущую активити
         finish()
     }
 }
