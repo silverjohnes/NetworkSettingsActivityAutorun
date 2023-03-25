512b030936dcd518e7ecf59ef9b9bddeeacea2ef
silverjohnes <silver.johnes@gmail.com>
2023-03-25 14:36:55 +0300

    Настройки вифи вместо панели с настройками сети.

--- app/src/main/java/silver/johnes/networksettingsactivityautorun/SilentRun.kt
+++ app/src/main/java/silver/johnes/networksettingsactivityautorun/SilentRun.kt
@@ -4,8 +4,8 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.provider.Settings
-import androidx.core.content.ContextCompat.startActivity
-import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.content.ContextCompat.startActivity
+//mport androidx.appcompat.app.AppCompatActivity
 
 class SilentRun : Service() {
     override fun onBind(intent: Intent): IBinder? {
@@ -14,7 +14,8 @@ class SilentRun : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         // код, который будет выполняться при старте сервиса
-        startActivity(Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        //startActivity(Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        startActivity(Intent(Settings.ACTION_WIFI_SETTINGS).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
 
         return START_STICKY
     }
