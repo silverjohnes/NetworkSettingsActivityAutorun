7e8320ada938ebc2ffb6ce797dd7eb41ec9ad127
silverjohnes <silver.johnes@gmail.com>
2023-04-07 15:30:30 +0300

    Закрытие Настроек если открыты перед открытием Настроек вифи

--- app/src/main/java/silver/johnes/networksettingsactivityautorun/MainActivity.kt
+++ app/src/main/java/silver/johnes/networksettingsactivityautorun/MainActivity.kt
@@ -2,12 +2,28 @@ package silver.johnes.networksettingsactivityautorun
 
 import android.content.Intent
 import android.os.Bundle
-//import android.provider.Settings
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //// Закрытие окна настроек если открыто
+        setContentView(R.layout.activity_main)
+
+        // Получаем контекст приложения
+        val context: Context = applicationContext
+
+        // Проверяем, запущено ли стандартное приложение Settings
+        val settingsIntent = Intent(Settings.ACTION_SETTINGS)
+        val isSettingsOpen = context.packageManager.queryIntentActivities(settingsIntent, 0).isNotEmpty()
+
+        // Если приложение Settings запущено, закрываем его
+        if (isSettingsOpen) {
+            finish()
+        }
+
 
         startService(Intent(this, SilentRun::class.java))
         finish()
--- app/src/main/java/silver/johnes/networksettingsactivityautorun/SilentRun.kt
+++ app/src/main/java/silver/johnes/networksettingsactivityautorun/SilentRun.kt
@@ -1,11 +1,9 @@
 package silver.johnes.networksettingsactivityautorun
 
 import android.app.Service
+import android.content.ComponentName
 import android.content.Intent
 import android.os.IBinder
-import android.provider.Settings
-//import androidx.core.content.ContextCompat.startActivity
-//mport androidx.appcompat.app.AppCompatActivity
 
 class SilentRun : Service() {
     override fun onBind(intent: Intent): IBinder? {
@@ -15,8 +13,11 @@ class SilentRun : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         // код, который будет выполняться при старте сервиса
         //startActivity(Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
-        startActivity(Intent(Settings.ACTION_WIFI_SETTINGS).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
-
+        //startActivity(Intent(Settings.ACTION_WIFI_SETTINGS).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        val intent = Intent()
+        intent.component = ComponentName("com.android.settings", "com.android.settings.Settings\$NetworkDashboardActivity")
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
         return START_STICKY
     }
 
